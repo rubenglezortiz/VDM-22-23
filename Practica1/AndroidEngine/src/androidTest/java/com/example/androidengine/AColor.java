0047d97e81f9496b5b8346bd9a3d84843cef0179
RTobar01 <rtobar01@ucm.es>
2022-10-20 11:09:09 +0200

    AColor

---
+++ Practica1/AndroidEngine/src/androidTest/java/com/example/androidengine/AColor.java
@@ -0,0 +1,28 @@
+package com.example.androidengine;
+
+import android.graphics.Color;
+
+import com.example.engine.IColor;
+
+
+public class AColor implements IColor {
+    Color miColor;
+
+    public AColor(int r_,int g_,int b_){
+        miColor = new Color();
+        miColor.red(r_);
+        miColor.blue(b_);
+        miColor.green(g_);
+    }
+    public AColor(int r_,int g_,int b_,int a_){
+        miColor = new Color();
+        miColor.red(r_);
+        miColor.blue(b_);
+        miColor.green(g_);
+        miColor.alpha(a_);
+    }
+
+    public Color getColor(){
+        return miColor;
+    }
+}
